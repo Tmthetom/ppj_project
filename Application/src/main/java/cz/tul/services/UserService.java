5b40693d3e2d064792fa0cb1d76a3f84c39a8a66
Tmthetom <moravec-tomas@seznam.cz>
2017-04-29 14:44:52 +0200

    Added UserService

---
+++ Application/src/main/java/cz/tul/services/UserService.java
@@ -0,0 +1,33 @@
+package cz.tul.services;
+
+import cz.tul.data.User;
+import cz.tul.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void create(User user) {
+        userRepository.save(user);
+    }
+
+    public boolean exists(User user) {
+        return userRepository.exists(user.getId_user());
+    }
+
+    public List<User> getAllUsers() {
+        return StreamSupport.stream(userRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    public void deleteUsers() {
+        userRepository.deleteAll();
+    }
+}
