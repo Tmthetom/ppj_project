0dfe357937b71451e338cfd50874d37f484b9a77
Tmthetom <moravec-tomas@seznam.cz>
2017-04-29 13:54:06 +0200

    Added ImageRepository Interface

---
+++ Application/src/main/java/cz/tul/repositories/ImageRepository.java
@@ -0,0 +1,20 @@
+package cz.tul.repositories;
+
+import cz.tul.data.Image;
+import cz.tul.data.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ImageRepository extends CrudRepository<Image, Integer> {
+    @Query("SELECT row FROM Image AS row WHERE row.id_image = :id_image")
+    public Image getImage(@Param("id_image") long id_image);
+
+    @Query("SELECT row FROM Image AS row WHERE row.user = :user")
+    public List<Image> findByAuthor(@Param("user") User user);
+
+    @Query("SELECT row FROM Image AS row WHERE row.name = :name")
+    public List<Image> findByName(@Param("name") String name);
+}
