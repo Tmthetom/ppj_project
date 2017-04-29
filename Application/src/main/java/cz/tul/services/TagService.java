64bcda581ce0e639ace0059b2dc5239f1a38d26d
Tmthetom <moravec-tomas@seznam.cz>
2017-04-29 14:47:37 +0200

    Added TagService

---
+++ Application/src/main/java/cz/tul/services/TagService.java
@@ -0,0 +1,33 @@
+package cz.tul.services;
+
+import cz.tul.data.Tag;
+import cz.tul.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class TagService {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    public void create(Tag tag) {
+        tagRepository.save(tag);
+    }
+
+    public boolean exists(Tag tag) {
+        return tagRepository.exists(tag.getName());
+    }
+
+    public List<Tag> getAll() {
+        return StreamSupport.stream(tagRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    public void deleteAll() {
+        tagRepository.deleteAll();
+    }
+}
