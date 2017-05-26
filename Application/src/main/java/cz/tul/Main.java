f355c7211b21710b579dc8985707d86b0749700d
Tmthetom <moravec-tomas@seznam.cz>
2017-05-26 17:42:41 +0200

    Updated Main -> Inifinite Loop

--- Application/src/main/java/cz/tul/Main.java
+++ Application/src/main/java/cz/tul/Main.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) throws Exception {
-        SpringApplication app = new SpringApplication(Main.class);
+        SpringApplication.run(Main.class, args);
     }
 }
